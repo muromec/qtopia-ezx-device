eddee31e3fe0ba678a5d5d29939eb4eb9dc6ee2e
Ilya Petrov <ilya.muromec@gmail.com>
2009-03-06 22:51:06 +0200

    update bt script

--- src/devtools/scripts/bt-poweron.sh
+++ src/devtools/scripts/bt-poweron.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+export PATH=/tmp/mbtdload/:$PATH
+[ -z "$BT_ADDR" ] && export BT_ADDR=0009DD502319
+
+mkdir /tmp/mbtdload/
+dd if=/dev/mtd1 of=/tmp/mbtdload/mbtdload bs=28808 count=1
+
+chmod +x /tmp/mbtdload/mbtdload
+
 hwtool -B
 ezx-hciattach -n
+
+rm -rf /tmp/mbtdload/
