4c951913cb5c0a57b1daccaf7aa06cd9f42af23e
Ilya Petrov <ilya.muromec@gmail.com>
2009-03-07 12:00:47 +0200

    detect mux_cli failure and restart in dummy mode

--- src/devtools/startup/qpe.sh
+++ src/devtools/startup/qpe.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-rm /tmp/restart-qtopia
-while true
+touch /tmp/restart-qtopia
+while [ -e /tmp/restart-qtopia ] 
 do
-	${0%.sh} $* 2>&1 | logger -t Qtopia
-	[ -e /tmp/restart-qtopia ] || break
+  ${0%.sh} -qws 2>&1 | logger -t Qtopia
+
+  if (dmesg | egrep -q 'DLCI.*timeout'); then
+    export QTOPIA_PHONE_DUMMY=1
+  fi
+  
 done
+
