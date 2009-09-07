e99ad133606fcbd4aacbe57af84e1d01d690e646
Ilya Petrov <ilya.muromec@gmail.com>
2009-09-08 02:16:11 +0300

    port vibro to openezx

--- src/plugins/phonevendors/ezx/ezxvibrateaccessory.cpp
+++ src/plugins/phonevendors/ezx/ezxvibrateaccessory.cpp
@@ -2,8 +2,16 @@
 #include <sys/ioctl.h>  
 #include <fcntl.h>
 #include <unistd.h>
+#include <QFile>
+#include <QTextStream>
 
 #include "ezxvibrateaccessory.h"
+#define VIB "/sys/bus/platform/devices/reg-virt-consumer.0/"
+#define VIB_OFF "0"
+#define VIB_ON "2000000"
+
+#define WO (QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate)
+
 /*
  *
  * Vibro
@@ -15,32 +23,43 @@ EZXVibrateAccessory::EZXVibrateAccessory
 {
     setSupportsVibrateOnRing( true );
     setSupportsVibrateNow( false );
+
+    QFile *max = new QFile();
+    max->setFileName(VIB"max_microvolts");
+    max->open(WO);
+
+    QTextStream o(max);
+    o << VIB_ON;
+
+    max->close();
+    delete max;
+
+    value = new QFile();
+    value->setFileName(VIB"min_microvolts");
+    value->open(WO);
+
 }
 
 EZXVibrateAccessory::~EZXVibrateAccessory()
 {
+    value->close();
+    delete value;
 }
 
-void EZXVibrateAccessory::setVibrateOnRing( const bool value )
+void EZXVibrateAccessory::setVibrateOnRing( const bool on )
 {
-    setVibrateNow(value);
+    setVibrateNow(on);
 }
 
-void EZXVibrateAccessory::setVibrateNow( const bool value )
+void EZXVibrateAccessory::setVibrateNow( const bool on )
 {
 
+  QTextStream o(value);
 
-  int level = 3; // TODO: is there vibro level in qtopia?
-
-  // open vibrator device and set mode
-  int vibro = open("/dev/vibrator", O_RDWR);
-  if (value)
-      ioctl(vibro, EZX_VIBRATOR_ENABLE,  &level);
+  if(on)
+    o << VIB_ON;
   else
-      ioctl(vibro, EZX_VIBRATOR_DISABLE, &level);
-  close(vibro);
-
-
+    o << VIB_OFF;
 
   QVibrateAccessoryProvider::setVibrateNow( value );
 }
